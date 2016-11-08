b22f53531dab66429690850a1170b8fa811e7143
Kai Volland <kaivolland@yahoo.de>
2016-11-08 15:14:04 +0100

    Adapt ci preparation scripts to 6.2.x versions.

--- ci/create-sencha-environment.sh
+++ ci/create-sencha-environment.sh
@@ -24,10 +24,10 @@ INSTALL_DIR="$TRAVIS_BUILD_DIR/ci/__install"
 SENCHA_CMD="$INSTALL_DIR/sencha"
 
 # The version of sencha command to downlaod and install
-SENCHA_CMD_VERSION="6.1.3.42"
+SENCHA_CMD_VERSION="6.2.0.103"
 
 # The version of ExtJS to download and configure the sencha workspace with
-SENCHA_EXTJS_VERSION="6.0.0"
+SENCHA_EXTJS_VERSION="6.2.0"
 
 #(if needed), will not fail if they are there already
 mkdir -p $DOWN_DIR
@@ -40,23 +40,23 @@ mkdir -p $INSTALL_DIR
 cd $DOWN_DIR
 
 # DOWNLOAD (if needed)
-# 1) Sencha cmd (v6.1.2.15)
+# 1) Sencha cmd (v6.2.0.103)
 if [ ! -f "SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh.zip" ]; then
     wget "http://cdn.sencha.com/cmd/$SENCHA_CMD_VERSION/no-jre/SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh.zip"
 fi
 
-# 2) Ext JS (v6.0.1)
+# 2) Ext JS (v6.2.0)
 if [ ! -f "ext-$SENCHA_EXTJS_VERSION-gpl.zip" ]; then
     wget "http://cdn.sencha.com/ext/gpl/ext-$SENCHA_EXTJS_VERSION-gpl.zip"
 fi
 
 # EXTRACT (if needed)
-# 1) Sencha cmd (v6.1.2.15)
+# 1) Sencha cmd (v6.2.0.103)
 if [ ! -f "SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh" ]; then
     unzip -q "SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh.zip"
 fi
 
-# 2) Ext JS (v6.0.1)
+# 2) Ext JS (v6.2.0)
 if [ ! -d "ext-$SENCHA_EXTJS_VERSION" ]; then
     unzip -q "ext-$SENCHA_EXTJS_VERSION-gpl.zip"
 fi
