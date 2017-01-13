b88c06e6e9e9b32224f35d4632e4d696ed405755
Kai Volland <kaivolland@yahoo.de>
2017-01-13 13:49:08 +0100

    Fix broken toast messages.

--- app/view/panel/application/GeneralController.js
+++ app/view/panel/application/GeneralController.js
@@ -3,20 +3,20 @@ Ext.define('MoMo.admin.view.panel.application.GeneralController', {
     alias: 'controller.momo-application-general',
 
     onPublicCheckboxChange: function(box) {
-        var me = this,
-            viewModel = me.getViewModel();
+        var me = this;
+        var viewModel = me.getView().lookupViewModel();
 
         if (box.checked) {
-            Ext.toast(viewModel.get('publicBoxToastText'), null, 'b');
+            Ext.toast(viewModel.get('general.publicBoxToastText'), null, 'b');
         }
     },
 
     onActiveCheckboxChange: function(box) {
-        var me = this,
-            viewModel = me.getViewModel();
+        var me = this;
+        var viewModel = me.getView().lookupViewModel();
 
         if (!box.checked) {
-            Ext.toast(viewModel.get('activeBoxToastText'), null, 'b');
+            Ext.toast(viewModel.get('general.activeBoxToastText'), null, 'b');
         }
     }
 
