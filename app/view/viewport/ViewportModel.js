7adc82fe904908606208b0a5b652d36e2fc3f287
Johannes Weskamm <weskamm@terrestris.de>
2017-05-12 10:36:07 +0200

    Fixed viewmodel formula by not using it...

--- app/view/viewport/ViewportModel.js
+++ app/view/viewport/ViewportModel.js
@@ -5,6 +5,7 @@ Ext.define('MoMo.admin.view.viewport.ViewportModel', {
     data: {
         currentView: null,
         user: null,
+        allowCreateOrEditWebmaps: false,
         i18n: {
             logoutTitle: 'Log out',
             logoutMessage: 'Are you sure you want to log out?'
@@ -20,20 +21,6 @@ Ext.define('MoMo.admin.view.viewport.ViewportModel', {
                 profileImage = 'resources/images/emptyUserAvatar.png';
             }
             return profileImage;
-        },
-        allowCreateOrEditWebmaps: function() {
-            var availableRoles = [];
-            if (this.get('user') && this.get('user').getData()) {
-                availableRoles = this.get('user').getData().groupRoles;
-            }
-            var isAllowed = false;
-            Ext.each(availableRoles, function(role) {
-                if (role.indexOf('ROLE_ADMIN') > -1 ||
-                    role.indexOf('ROLE_SUBADMIN') > -1) {
-                    isAllowed = true;
-                }
-            });
-            return isAllowed;
         }
     }
 });
